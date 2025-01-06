d6f0460cf4aa829596b8037b958700f8b4393036
JamesHF123 <57965895+JamesHF123@users.noreply.github.com>
2025-01-06 12:28:46 +0000

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -38,7 +38,7 @@ api.add_listener(
     CheckiOReferee(
         tests=TESTS,
         function_name={
-            "python": "sum_two",
+            "python": "firstname",
             "js": "sumTwo"
         },
         cover_code={
